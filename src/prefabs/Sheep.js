4d1050c07598ca8636488ddb9cc971866958c09c
andib7 <andibarron7@gmail.com>
2022-05-02 09:29:16 -0700

    Update

--- src/prefabs/Sheep.js
+++ src/prefabs/Sheep.js
@@ -2,19 +2,29 @@ class Sheep extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame, pointValue) {
         super(scene, x, y, texture, frame);
         scene.add.existing(this); //add to scene
+        this.resetX = x; 
         this.moveSpeed = 1; //pixels/frame
         this.inScene =  false;
         this.points = pointValue;
     }
 
     update(dogX) {
-        if (this.x > dogX && this.x <= game.config.width - this.width*1.6 && this.x - dogX <= 200) {
+        if (this.x > dogX && this.x <= game.config.width - this.width*1.6 && this.x - dogX <= 150) {
             this.x += this.moveSpeed;
         }
-        else if (this.x < dogX && this.x >= this.width*1.5 && dogX - this.x <= 200) {
+        else if (this.x < dogX && this.x >= this.width*1.5 && dogX - this.x <= 150) {
             this.x -= this.moveSpeed;
         }
         
     }
 
+    enterScene(){
+        
+        if (this.x > game.config.width / 2) {
+            this.x -= this.moveSpeed;
+        }
+        else {
+            this.x += this.moveSpeed;
+        }
+    }
 }
